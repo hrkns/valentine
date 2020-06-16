d3e33d2dabc663780f8f9b3d5d90b7412f758af6
José Rosendo <jose.l.rosendo.s@gmail.com>
2020-06-16 00:05:03 -0300

    Set and Get work periods start date. Update project README. Update Firebase dependencies. Create batch script for firebase functions deployment.

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
+
+**Instructions:**
+----------------
 * For the first time, run:
   * `npm install -g firebase-tools`
-* Run the application with `ionic serve`
+* Run `npm install`.
+* Run the application with `ionic serve`.
 
-For development:
-* Remember to run `firebase login` when you have to work on Firebase paltform.
+**For development:**
+----------------
+* Remember to run `firebase login` when you have to work on **Firebase** platform.
+* List of tasks in **Firebase** (remember to enter first to the `firebase` directory):
+  * **Functions deployment:** `deploy_functions` or `firebase deploy --only functions`.
+  
--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,8 +1,55 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const request = functions.https.onRequest;
+
+const httpResponse = (error:any, response:functions.Response, message?:string|object) => {
+  const status = error ? 400 : 200;
+  const msg = error ? error : message;
+  response.status(status).send(msg);
+}
+
+const ref = (_ref: string) => {
+  return admin.database().ref(`/${_ref}`);
+}
+
+const workPeriodsStartDateCollectionName = 'work-periods-start-date';
+
+const getFromDbWorkPeriodsStartDate = (callback:any) => {
+
+  ref(workPeriodsStartDateCollectionName)
+    .limitToFirst(1)
+    .on('value', callback);
+};
+
+export const setWorkPeriodsStartDate = request(async (req, res) => {
+
+    const payload = {
+      start_date : req.body.start_date,
+    };
+
+    // tslint:disable-next-line: no-floating-promises
+    ref(workPeriodsStartDateCollectionName)
+      .remove(() => {
+
+        ref(workPeriodsStartDateCollectionName).push({
+          start_date: payload.start_date,
+        }, error => {
+    
+          httpResponse(error, res, 'Establecido inicio de ciclos de 4 dias.');
+        });
+      });
+});
+
+export const getWorkPeriodsStartDate = request(async (req, res) => {
+
+  getFromDbWorkPeriodsStartDate((value:any) => {
+
+    value.forEach((first:any) => {
+
+      httpResponse(null, res, first);
+    });
+  });
+});
